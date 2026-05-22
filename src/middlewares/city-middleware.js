f35a966b9e06fe6d83fb91f479fe741b54d415fc
Sriram-Kommmana <kommanamarirs@gmail.com>
2026-05-22 17:33:31 +0530

    Added middleware for createCity api

--- src/middlewares/city-middleware.js
+++ src/middlewares/city-middleware.js
@@ -3,11 +3,11 @@ const { ErrorResponse } = require('../utils/common');
 const AppError = require('../utils/errors/app-error');
 
 function validateCreateRequest(req, res, next) {
-    if(!req.body.modelNumber)
+    if(!req.body || !req.body.name)
     {   
-        ErrorResponse.message = 'Something went wrong while creating airplane';
+        ErrorResponse.message = 'Something went wrong while creating city';
         
-        ErrorResponse.error = new AppError(['model number is not found in the oncoming request'], StatusCodes.BAD_REQUEST);
+        ErrorResponse.error = new AppError(['city name is not found in the oncoming request'], StatusCodes.BAD_REQUEST);
         
         return res
             .status(StatusCodes.BAD_REQUEST)
--- src/routes/v1/city-routes.js
+++ src/routes/v1/city-routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const {CityController} = require("../../controllers");
+const {CityMiddlewares} = require("../../middlewares");
 
 
-router.post('/', CityController.createCity);
+router.post('/', CityMiddlewares.validateCreateRequest, CityController.createCity);
 
 
 module.exports = router;
