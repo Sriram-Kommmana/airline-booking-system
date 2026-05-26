8121c01513abbfd1d23cf62258465fef00f206ef
Sriram-Kommmana <kommanamarirs@gmail.com>
2026-05-26 16:02:16 +0530

    Added Airport resource crud

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,9 +3,11 @@ const {InfoController} = require('../../controllers');
 const router = express.Router();
 const airplaneRoutes = require('./airplane-routes');
 const cityRoutes = require('./city-routes');
+const airportRoutes = require('./airport-routes');
 
 router.use('/airplanes', airplaneRoutes);
 router.use('/cities', cityRoutes);
 router.get('/info', InfoController.info)
+router.use('/airports', airportRoutes);
 
 module.exports = router;
